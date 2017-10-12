c9a2b090b358cbf08a086ca7880a23936e490be8
Mats Jacobsson <Mats.Jacobsson@qlik.com>
2017-10-12 14:58:49 +0200

    Add Go tests.

--- examples/go/basic_example/server.go
+++ examples/go/basic_example/server.go
@@ -30,7 +30,7 @@ var (
 	tls      = flag.Bool("tls", false, "Connection uses TLS if true, else plain TCP")
 	certFile = flag.String("cert_file", "", "The TLS cert file")
 	keyFile  = flag.String("key_file", "", "The TLS key file")
-	port     = flag.Int("port", 50051, "The server port")
+	port     = flag.Int("port", 50055, "The server port")
 )
 
 // Function definitions.
